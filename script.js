3ca0aebd22b106ddfe6c70f51919f96f3f2e02fa
lebowvsky <lebowvsky@gmail.com>
2020-03-21 00:45:28 +0100

    Mise en place d'une fonction d'apparition au scroll

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ function moveMenu(){
 
 burger.addEventListener("click", moveMenu);
 
+/* Questionnaire */
 
+const question1 = document.getElementsByName('question1');
+const question2 = document.getElementsByName('question2');
+const question3 = document.getElementsByName('question3');
+const question4 = document.getElementsByName('question4');
+const question5 = document.getElementsByName('question5');
+
+const scoreFunction = () => {const result = parseInt(question1.value || 0)
+        + parseInt(question2.value || 0) 
+        + parseInt(question3.value || 0)
+        + parseInt(question4.value || 0)
+        + parseInt(question5.value || 0)
+        console.log(document.getElementsByName('score').innerHTML = `${result}`);
+
+};
 /* ------------ GoogleMap---------------------------- */
 const mapButton = document.body.querySelector('#map-button');
 const googleMap = document.body.querySelector('#google-map');
 if(mapButton) {
-    const mapButton = document.body.querySelector('#map-button');
-    const googleMap = document.body.querySelector('#google-map');
-
     function mapShow() {
         googleMap.classList.replace('hidden', 'visible');
     }
@@ -40,12 +52,34 @@ const cards = document.body.querySelectorAll('.card');
     elt.style.transition = "opacity 450ms ease-in-out";
     window.setTimeout(()=>{
         elt.style.opacity = "1";
-    }, 1000);
+    }, 500);
 }); */
 
+
 /* ------------ Scroll Event ---------------------------- */
-const screenHeight = screen.height;
-console.log("Hauteur écran : "+screen.height);
+const docHeight = document.documentElement.clientHeight; //hauteur totale du doc
+
 cards.forEach(elt => {
-    console.log(elt.offsetTop);
+    const eltPositionTop = elt.offsetTop;
+    if((eltPositionTop) > docHeight - (docHeight/4)){    // Si la card est en dessous de 3/4 de l'écran alors card pas visible
+        elt.style.transition = "opacity 350ms ease-in-out";
+        elt.style.opacity = "0";
+    } else {elt.style.opacity = "1";}
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.addEventListener('scroll', function(){
+        const eltScrollTop = document.documentElement.scrollTop; // pixels à scroller jusqu'à fin du doc
+        
+
+        cards.forEach(elt => {
+            const eltPositionTop = elt.offsetTop;
+            if((eltPositionTop - eltScrollTop) > docHeight - (docHeight/4)){    // Si la card passe au dessus de 3/4 de l'écran alors card visible
+                elt.style.transition = "opacity 350ms ease-in-out";
+                elt.style.opacity = "0";
+            } else {elt.style.opacity = "1";}
+        })
+
+
+    });
 });
