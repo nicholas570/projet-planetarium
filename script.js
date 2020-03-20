e6071e5d9e69ba0543f30a0ea7b469155a14b726
lebowvsky <lebowvsky@gmail.com>
2020-03-20 12:26:48 +0100

    modif de script.js pour merge sur dev

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ function moveMenu(){
 
 burger.addEventListener("click", moveMenu);
 
+/* Questionnaire */
 
+const question1 = document.getElementsByName('question1');
+const question2 = document.getElementsByName('question2');
+const question3 = document.getElementsByName('question3');
+const question4 = document.getElementsByName('question4');
+const question5 = document.getElementsByName('question5');
+
+const scoreFunction = () => {const result = parseInt(question1.value || 0)
+        + parseInt(question2.value || 0) 
+        + parseInt(question3.value || 0)
+        + parseInt(question4.value || 0)
+        + parseInt(question5.value || 0)
+        console.log(document.getElementsByName('score').innerHTML = `${result}`);
+
+};
 /* ------------ GoogleMap---------------------------- */
 const mapButton = document.body.querySelector('#map-button');
 const googleMap = document.body.querySelector('#google-map');
 if(mapButton) {
-    const mapButton = document.body.querySelector('#map-button');
-    const googleMap = document.body.querySelector('#google-map');
-
     function mapShow() {
         googleMap.classList.replace('hidden', 'visible');
     }
@@ -43,6 +55,10 @@ cards.forEach(elt => {
     }, 1000);
 });
 
+/* ------------ Scroll Event ---------------------------- */
+const screenHeight = screen.height;
 
-
+cards.forEach(elt => {
+    console.log(elt.offsetTop);
+});
 
