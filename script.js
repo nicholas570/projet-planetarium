a83146b42768eecd3f9afe0bf61dec8c2f0cadfe
lebowvsky <lebowvsky@gmail.com>
2020-03-20 11:22:36 +0100

    Fonctionnalité d'apparition des cards après 1 seconde

--- script.js
+++ script.js
@@ -11,18 +11,38 @@ burger.addEventListener("click", moveMenu);
 
 
 /* ------------ GoogleMap---------------------------- */
-
 const mapButton = document.body.querySelector('#map-button');
 const googleMap = document.body.querySelector('#google-map');
+if(mapButton) {
+    const mapButton = document.body.querySelector('#map-button');
+    const googleMap = document.body.querySelector('#google-map');
 
-function mapShow() {
-    googleMap.classList.replace('hidden', 'visible');
+    function mapShow() {
+        googleMap.classList.replace('hidden', 'visible');
+    }
+
+    function mapHide() {
+        googleMap.classList.replace('visible', 'hidden');
+    }
+
+    mapButton.addEventListener('click', mapShow);
+    googleMap.addEventListener('click', mapHide);
+
 }
 
-function mapHide() {
-    googleMap.classList.replace('visible', 'hidden');
-}
 
-mapButton.addEventListener('click', mapShow);
-googleMap.addEventListener('click', mapHide);
+/* ------------ Cards apparitions---------------------------- */
+
+const cards = document.body.querySelectorAll('.card');
+
+cards.forEach(elt => {
+    elt.style.opacity = "0";
+    elt.style.transition = "opacity 450ms ease-in-out";
+    window.setTimeout(()=>{
+        elt.style.opacity = "1";
+    }, 1000);
+});
+
+
+
 
